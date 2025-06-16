a89cc93002337a4b27491b0c4a76f4ca635a6dab
Estefany92 <ashley.huertas@udla.edu.ec>
2025-06-15 21:01:19 -0500

    Agregar archivos de proyecto.

---
+++ About.cs
@@ -0,0 +1,9 @@
+﻿namespace Notes.Models;
+
+internal class About
+{
+    public string Title => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://aka.ms/maui";
+    public string Message => "Estefani Huertas, 20 años Ing. de Software";
+}
