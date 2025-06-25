e65f7f9929bbdffccebba6052b65a017a289aa77
Estefany92 <ashley.huertas@udla.edu.ec>
2025-06-24 23:40:46 -0500

    finalizacion de mvvm notes funcional

--- About.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Notes.Models;
-
-internal class About
-{
-    public string Title => AppInfo.Name;
-    public string Version => AppInfo.VersionString;
-    public string MoreInfoUrl => "https://aka.ms/maui";
-    public string Message => "Estefani Huertas, 20 años Ing. de Software";
-}
